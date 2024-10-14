06cee6a2c37aee7954c3a454624c221e11f3de9c
jlanusmartinez <jlanusmartinez@itba.edu.ar>
2024-10-14 18:14:26 -0300

    Create class_Contacto.py

---
+++ class_Contacto.py
@@ -0,0 +1,17 @@
+class Contacto :
+    
+    def __init__(self,nombre , numero_telefono):
+        self.nombre=nombre
+        self.numero_telefono=numero_telefono
+    
+    def set_nombre(self,nuevo_nom):
+        self.nombre=nuevo_nom
+        
+        
+    def set_numero(self,nuevo_num):
+        self.numero_telefono=nuevo_num  
+    
+    def __str__(self):
+        return f"Contacto({self.nombre}, {self.numero_telefono})"
+          
+    
