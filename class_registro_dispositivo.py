5c951c6716087eae866ec87522e5fe0c20fbfc4c
jlanusmartinez <jlanusmartinez@itba.edu.ar>
2024-11-11 22:11:42 -0300

    .

--- class_registro_dispositivo.py
+++ class_registro_dispositivo.py
@@ -11,17 +11,21 @@ class Registro_dispositivos (dict):
        except Exception as e:
         print('No hay archivo para leer')
         print(e)
+        
+    # recupera los datos del archivo y los guarda en el diccionario
 
     def inicializar (self,diccionario:dict):
             for key , values in diccionario.items():
                 self[key]=values
             Celular.inicializar_ids(diccionario.values())
-                
+    
+    # lee los datos del archivo y los guarda en el diccionario con inicializar
     def leer(self):
          with open('dispositivos_registrados.pkl','rb') as f:
               registro=pickle.load(f)
               self.inicializar(registro)
 
+    # almacena los datos en el archivo pickle
     def guardar(self): 
         if self.values(): 
             with open('dispositivos_registrados.pkl','wb') as f:
